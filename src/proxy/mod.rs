f146ff8b5fec3b892909beba398bb33c2a7bf4a9
Жора Змейкин <katze@etersoft.ru>
2026-02-14 02:11:13 +0300

    Unix socket listener + reverse proxy improvements

--- src/proxy/mod.rs
+++ src/proxy/mod.rs
@@ -2,10 +2,12 @@
 
 pub mod handshake;
 pub mod client;
+pub(crate) mod direct_relay;
+pub(crate) mod middle_relay;
 pub mod relay;
 pub mod masking;
 
 pub use handshake::*;
-pub use client::ClientHandler;
+pub use client::{ClientHandler, handle_client_stream};
 pub use relay::*;
 pub use masking::*;
